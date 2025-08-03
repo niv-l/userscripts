b7402fbaf2fd7ffbf5edb24cdb77939485efe26f
Nivyan Lakhani <mail@nivyan.xyz>
2025-08-03 13:29:17 +0100

    Fixed regex

--- lmarena/keybinds.js
+++ lmarena/keybinds.js
@@ -3,8 +3,8 @@
 // @version      0.1
 // @description  Adds keybinds to trigger search. I'd rather not use my mouse.
 // @author       Nivyan Lakhani
-// @match        https://beta.lmarena.ai/?mode=side-by-side
-// @match        https://lmarena.ai/?mode=side-by-side
+// @match        https://beta.lmarena.ai/*
+// @match        https://lmarena.ai/*
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
