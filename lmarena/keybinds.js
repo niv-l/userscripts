17de1dd14e9499a9dffb2bc646d74b3a8f31ba88
Nivyan Lakhani <mail@nivyan.xyz>
2025-08-03 13:26:54 +0100

    Initial commit + added metadata

---
+++ lmarena/keybinds.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         LMArena Extra Keybinds
+// @version      0.1
+// @description  Adds keybinds to trigger search. I'd rather not use my mouse.
+// @author       Nivyan Lakhani
+// @match        https://beta.lmarena.ai/?mode=side-by-side
+// @match        https://lmarena.ai/?mode=side-by-side
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
