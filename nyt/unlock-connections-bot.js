addbf61e1e691fe18d9bb1198f4a749e3437be44
Nivyan Lakhani <mail@nivyan.xyz>
2025-09-13 20:27:29 +0100

    Added description.

---
+++ nyt/unlock-connections-bot.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Unlock Connections Bot
+// @description  Bypasses the paywalled connection bot analysis
+// @author       Nivyan Lakhani
+// @match        https://www.nytimes.com/games/connections
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
+// TODO: We take advantage of the fact that the connection bot button is still clickable despite it being obfuscated by elements that block clicks.
